c843eb4c551e20a927bb499e53ea8aa13fe2d8b7
Kelwin <kelwin.sbastos21@gmail.com>
2026-06-11 13:08:34 -0300

    feat: Create User class

---
+++ src/br/com/totvschallenge/entities/Usuario.java
@@ -0,0 +1,50 @@
+package br.com.totvschallenge.entities;
+
+public class Usuario {
+    private String nome;
+    private String emailComercial;
+    private String senha;
+
+    public Usuario(String nome, String emailComercial, String senha) {
+        this.nome = nome;
+        this.emailComercial = emailComercial;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmailComercial() {
+        return emailComercial;
+    }
+
+    public void setEmailComercial(String emailComercial) {
+        this.emailComercial = emailComercial;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void logar(String nome, String email, String senha){
+        while(true) {
+            if (!nome.equalsIgnoreCase(getNome())) {
+                continue;
+            } else if (!email.equalsIgnoreCase(getSenha())) {
+                continue;
+            } else if(!senha.equalsIgnoreCase(getSenha())){
+                continue;
+            }
+            break;
+        }
+    }
+}
